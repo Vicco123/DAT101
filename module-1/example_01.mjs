da7a1907bc3471c77b3d709a47930a4afa629ec1
Victor Alfred <victorandre1122@gmail.com>
2026-08-28 15:55:53 +0200

    oppgave 2

--- module-1/example_01.mjs
+++ module-1/example_01.mjs
@@ -1,2 +1,10 @@
 "use strict"
 import { printOut } from "../../common/script/utils.mjs";
+
+let a = 5;
+const text = `Variable a inneholder ${a}`;
+const l = text.length;
+printOut( l );
+const text2 = text.substring(0, 10);
+printOut(text);
+printOut(text2);
